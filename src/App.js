622bf3e372a34b97bb883a6b9285e7235894fa22
monica <monica.zucca1974@gmail.com>
2026-09-08 09:54:10 +0200

    Aggiungo stile alla pagina e corretto errore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Qui e ora</h1>
-      <p>La storia a portata di mano</p>
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1 style={{ color: '#2c3e50' }}>Qui e ora</h1>
+      <p style={{ fontSize: '18px', color: '#555' }}>La storia a portata di mano</p>
+      <p style={{ marginTop: '30px', color: '#888' }}>Scopri cosa è successo nel luogo dove ti trovi</p>
     </div>
   );
 }
+
 export default App;
